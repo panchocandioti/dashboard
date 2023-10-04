b449326780428face287b53528e413e7942c9901
panchocandioti <panchocandioti@gmail.com>
2023-10-03 21:38:12 -0300

    Maqueta básica terminada

--- src/Components/HumedadPresion.js
+++ src/Components/HumedadPresion.js
@@ -5,11 +5,11 @@ function HumedadPresion() {
     <div className='subseccion'>
         <div className='elemento' style={{color: "darkblue"}}>
             <p><b>Humedad relativa</b></p>
-            <p><b>90%</b></p>
+            <p style={{color: "white", backgroundColor: "darkblue"}}><b>90%</b></p>
         </div>
         <div className='elemento' style={{color: "darkred"}}>
         <p><b>Presión atmosférica</b></p>
-            <p><b>1014 kP</b></p>
+            <p style={{color: "white", backgroundColor: "darkred"}}><b>1014 kP</b></p>
         </div>
     </div>
   )
