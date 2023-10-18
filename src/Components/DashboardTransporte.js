78db34b69a6ae1b40040b2bf6b37089039729b23
panchocandioti <panchocandioti@gmail.com>
2023-10-17 21:27:09 -0300

    Pruebas con mapa y json transporte

--- src/Components/DashboardTransporte.js
+++ src/Components/DashboardTransporte.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { datosTransporte } from "./DatosTransporte";
 
 function DashboardTransporte() {
+
+    const position = [datosTransporte[0]["latitude"], datosTransporte[0]["longitude"]];
+
     return (
         <div>
-            <p>MITAD 2: PRÓXIMAMENTE DASHBOARD TRANSPORTE</p>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
-    );
+    )
 };
 
 export default DashboardTransporte;
