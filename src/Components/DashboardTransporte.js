7cfd4e68119d431a9de3c60ae60cce3f819a1557
panchocandioti <panchocandioti@gmail.com>
2023-10-24 23:11:11 -0300

    Pruebas con mapa y json transporte

--- src/Components/DashboardTransporte.js
+++ src/Components/DashboardTransporte.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import { datosTransporte } from "./DatosTransporte";
 
 function DashboardTransporte() {
 
-    const position = [datosTransporte[0]["latitude"], datosTransporte[0]["longitude"]];
+    const [linea, setLinea] = useState('22');
+    const [datosTransporteFiltro, setDatosTransporteFiltro] = useState(datosTransporte.filter((item) => item.route_id === '22'));
+    const [centrado, setCentrado] = useState([-34.63155, -58.4261055]);
+    const [id, setId] = useState("1")
 
-    return (
-        <div>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </div>
-    )
-};
+    const handleSelectChange = (event) => {
+        setLinea(event.target.value);
+    };
 
-export default DashboardTransporte;
+    useEffect(() => {
+        setDatosTransporteFiltro(datosTransporte.filter((item) => item.route_id === linea));
+        setId(datosTransporteFiltro[0]["id"]);
+        setCentrado([datosTransporteFiltro[0]["latitude"], datosTransporteFiltro[0]["longitude"]]);
+    }, [linea])
+
+        return (
+            <div>
+                <div style={{backgroundColor: "#ccc", border: "2px solid #000"}}>
+                    <label htmlFor="opcionesDropdown">Selecciona una línea: </label>
+                    <select id="opcionesDropdown" value={linea} onChange={handleSelectChange}>
+                        <option value="">Selecciona una opción</option>
+                        {datosTransporte.map((opcion) => (
+                            <option key={opcion.id} value={opcion.route_id}>
+                                {opcion.route_id}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <MapContainer key={id} center={centrado} zoom={13} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {datosTransporteFiltro.map((item, index) => {
+                        return (<Marker position={[item["latitude"], item["longitude"]]}>
+                            <Popup>
+                                <>{[item["route_id"]]}</>
+                                <br></br>
+                                <>{[item["trip_headsign"]]}</>
+                            </Popup>
+                        </Marker>)
+                    })}
+
+                </MapContainer>
+            </div>
+        )
+    };
+
+    export default DashboardTransporte;
