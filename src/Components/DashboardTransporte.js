7ec4ab181cae659f8d57f04c270e6909f249f183
panchocandioti <panchocandioti@gmail.com>
2023-10-28 17:54:57 -0300

    centrado del mapa

--- src/Components/DashboardTransporte.js
+++ src/Components/DashboardTransporte.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvent, animateRef } from 'react-leaflet'
 import { datosTransporte } from "./DatosTransporte";
 
 function DashboardTransporte() {
 
     const [linea, setLinea] = useState('1');
-    const [transportData, setTransportData] = useState(datosTransporte.filter((item) => item.route_id == '1'));
-    const [centrado, setCentrado] = useState([]);
+    const [transportData, setTransportData] = useState(datosTransporte.filter((item) => item.route_id === '1'));
+    const [centrado, setCentrado] = useState([-34.64359, -58.47478]);
     const [cargando, setCargando] = useState(true);
+
+    const [latitudes, setLatitudes] = useState(transportData.map(item => item.latitude));
+    const [longitudes, setLongitudes] = useState(transportData.map(item => item.longitude));
+    const [centerLat, setCenterLat] = useState((Math.max(...latitudes) + Math.min(...latitudes)) / 2);
+    const [centerLng, setCenterLng] = useState((Math.max(...longitudes) + Math.min(...longitudes)) / 2);
+    const [selectedData, setSelectedData] = useState(transportData.find(item => item.route_id === linea));
 
     const datosDropDown1 = datosTransporte.map(item => item.route_id);
     const datosDropDown2 = datosDropDown1.sort((a, b) => a - b);
     const datosDropdown3 = [...new Set(datosDropDown2)];
+    const formatoCoordenada = /^-?\d+(\.\d+)?$/;
 
     const handleSelectChange = (event) => {
         const selectedLinea = event.target.value;
@@ -29,30 +36,31 @@ function DashboardTransporte() {
     };
 
     useEffect(() => {
-        let latitudes = transportData.map(item => item.latitude);
-        let longitudes = transportData.map(item => item.longitude);
-        let centerLat = (Math.max(...latitudes) + Math.min(...latitudes)) / 2;
-        let centerLng = (Math.max(...longitudes) + Math.min(...longitudes)) / 2;
-        let selectedData = transportData.find(item => item.route_id === linea);
-        if (selectedData) {
-            setCentrado([centerLat, centerLng]);
+        setLatitudes(transportData.map(item => item.latitude));
+        setLongitudes(transportData.map(item => item.longitude));
+        setCenterLat((Math.max(...latitudes) + Math.min(...latitudes)) / 2);
+        setCenterLng((Math.max(...longitudes) + Math.min(...longitudes)) / 2);
+        setSelectedData(transportData.find(item => item.route_id === linea));
+        if (!selectedData || !formatoCoordenada.test(centerLat) || !formatoCoordenada.test(centerLng)) {
+            setCentrado([-34.64359, -58.47478])
         }
-    }, [linea]);
+        else { setCentrado([centerLat, centerLng]); }
+    }, [transportData, linea]);
 
     useEffect(() => {
         const fetchTransportData = async () => {
-          try {
-            const response = await fetch(`https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?route_id=${linea}&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6`);
-            const data = await response.json();
-            setTransportData(data);
-          } catch (error) {
-            console.error(error);
-          }
+            try {
+                const response = await fetch(`https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?route_id=${linea}&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6`);
+                const data = await response.json();
+                setTransportData(data);
+            } catch (error) {
+                console.error(error);
+            }
         };
         fetchTransportData();
         const interval = setInterval(fetchTransportData, 31000);
         return () => clearInterval(interval);
-      }, [linea]);
+    }, [linea]);
 
     useEffect(() => {
         async function fetchData() {
@@ -61,6 +69,14 @@ function DashboardTransporte() {
         }
         fetchData();
     }, []);
+
+    const RecenterAutomatically = () => {
+        const map = useMap();
+        useEffect(() => {
+            map.setView(centrado);
+        }, [transportData, linea]);
+        return null;
+    }
 
     return (
         <div>
@@ -81,15 +97,14 @@ function DashboardTransporte() {
                     </div>
                 </div>
 
-
-
                 <MapContainer center={centrado} zoom={11} scrollWheelZoom={true}>
+                    <RecenterAutomatically position={centrado}  />
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     {transportData.map((item, index) => {
-                        return (<Marker key={item["id"]} position={[item["latitude"], item["longitude"]]}>
+                        return (<Marker key={item["id"] || index} position={[item["latitude"], item["longitude"]]}>
                             <Popup>
                                 <>Ruta: {[item["route_id"]]}</>
                                 <br></br>
