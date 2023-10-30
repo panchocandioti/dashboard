2139d278081ce988261571092ca8efd872724ea4
panchocandioti <panchocandioti@gmail.com>
2023-10-30 12:44:00 -0300

    centrado del mapa

--- src/Components/DashboardTransporte.js
+++ src/Components/DashboardTransporte.js
@@ -98,7 +98,7 @@ function DashboardTransporte() {
                 </div>
 
                 <MapContainer center={centrado} zoom={11} scrollWheelZoom={true}>
-                    <RecenterAutomatically position={centrado}  />
+                    <RecenterAutomatically position={centrado} />
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
